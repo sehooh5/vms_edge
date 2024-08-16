ab8ce758552d1d14c5fbe3efb6d82f06de4d71e8
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2024-08-16 16:03:42 +0900

    delete etc modules

--- Project_Edge/vms_server.py
+++ Project_Edge/vms_server.py
@@ -5,30 +5,30 @@ import json
 import response
 
 
-# sudo 사용으로 k8s config 설정 파일 위치 지정해주기
-os.environ['KUBECONFIG'] = '/home/edge-master-01/.kube/config'
-
-app = Flask(__name__)
-app.config['JSON_AS_ASCII'] = False  # jsonify 한글깨짐 해결
-CORS(app)
-
-# 다른 서버에 명령 보낼때 사용
-cli = paramiko.SSHClient()
-cli.set_missing_host_key_policy(paramiko.AutoAddPolicy)
-
-# 랜덤한 문자열 생성기
-_LENGTH = 4
-string_pool = string.ascii_letters + string.digits
-
-API_URL = "http://123.214.186.244:4880"
-
-# IP 주소
-ips = subprocess.check_output("hostname -I", shell=True).decode('utf-8')
-ip = ips.split(' ')[0]
-
-# host name
-user_name = os.getlogin()
-print(user_name)
+# # sudo 사용으로 k8s config 설정 파일 위치 지정해주기
+# os.environ['KUBECONFIG'] = '/home/edge-master-01/.kube/config'
+#
+# app = Flask(__name__)
+# app.config['JSON_AS_ASCII'] = False  # jsonify 한글깨짐 해결
+# CORS(app)
+#
+# # 다른 서버에 명령 보낼때 사용
+# cli = paramiko.SSHClient()
+# cli.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+#
+# # 랜덤한 문자열 생성기
+# _LENGTH = 4
+# string_pool = string.ascii_letters + string.digits
+#
+# API_URL = "http://123.214.186.244:4880"
+#
+# # IP 주소
+# ips = subprocess.check_output("hostname -I", shell=True).decode('utf-8')
+# ip = ips.split(' ')[0]
+#
+# # host name
+# user_name = os.getlogin()
+# print(user_name)
 
 @ app.route('/')
 def index():
